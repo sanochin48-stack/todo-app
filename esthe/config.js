501cd59314dacf121ea63a804a3a594c8c3907fb
sanochin48-stack <sanochin48@gmail.com>
2026-09-24 08:00:20 +0900

    Point esthe app to latest GAS deployment

--- esthe/config.js
+++ esthe/config.js
@@ -1,6 +1,6 @@
 // Public configuration only. Never put credentials, emails, or spreadsheet IDs here.
 window.ESTHE_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbxlB31-Zz5GZCxBgAyy0_3kit9i72-vuNM4zAxC7lwtfrGM7nIjmG0lyVi3gQWwyZ3f/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxq1gyOime2rAxFLYNGtJPP_W8K-a2E9Fyv3koN3Ni3qmSqAoc9GRJoJuOnBv39OFYGOQ/exec',
   googleClientId: '321256439173-7b83l4fivrghm80kmlhhrusobbrpv4u8.apps.googleusercontent.com', // Public OAuth client ID, not a client secret.
   timeoutMs: 45000
 });
